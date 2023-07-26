bd03245987f458b547903d96028f0ed0b1a451c1
Leonard Faecke <leo.faecke@outlook.de>
2023-07-26 18:26:20 +0200

    Added more info, set up original mod file

---
+++ src/main/java/net/lordbowser/pixelmonquester/PixelmonQuester.java
@@ -0,0 +1,15 @@
+package net.lordbowser.pixelmonquester;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * This class is the main mod file
+ */
+@Mod(PixelmonQuester.MOD_ID)
+public class PixelmonQuester {
+
+    /** This is the id of the mod */
+    public static final String MOD_ID = "pixelmonquester";
+
+
+}
